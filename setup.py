ec701bb15c32d8e1bdb172a5991c19b9a9021134
alshund <shundrikaleksey@gmail.com>
2020-08-27 12:02:07 +0300

    rl-437/compare Compare: - downgrade setup package level

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "regula.face.webclient.gen"
+NAME = "regula.face.webclient"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
