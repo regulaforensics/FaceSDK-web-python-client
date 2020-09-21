c7d08cb5b750bf72d6fcc0b7588315477ad4fdd5
alshund <shundrikaleksey@gmail.com>
2020-09-21 12:49:06 +0300

    rl-440/detect Detect: - add empty image condition for ext.DetectRequest - add hints for ext.MatchingApi detect method

--- regula/facerecognition/webclient/ext/api/matching_api.py
+++ regula/facerecognition/webclient/ext/api/matching_api.py
@@ -23,4 +23,30 @@ class MatchingApi(GenMatchingApi):
         return super().compare(compare_request, **kwargs)
 
     def detect(self, detect_request, **kwargs) -> DetectResponse:
+        """Detect facial coordinates.
+
+        :param detect_request: (required)
+        :type detect_request: DetectRequest
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _return_http_data_only: response data without head status code
+                                       and headers
+        :type _return_http_data_only: bool, optional
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :type _preload_content: bool, optional
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the authentication
+                              in the spec for a single request.
+        :type _request_auth: dict, optional
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: tuple(DetectResponse, status_code(int), headers(HTTPHeaderDict))
+        """
         return super().detect(detect_request, **kwargs)
--- regula/facerecognition/webclient/ext/models/detect_request.py
+++ regula/facerecognition/webclient/ext/models/detect_request.py
@@ -1,11 +1,14 @@
-from typing import Optional, Union
+from typing import Union
 
 from regula.facerecognition.webclient.ext.common import Base64String, bytes_to_base64
-from regula.facerecognition.webclient.gen import DetectRequest as GetDetectRequest
+from regula.facerecognition.webclient.gen import DetectRequest as GetDetectRequest, ApiValueError
 
 
 class DetectRequest(GetDetectRequest):
-    def __init__(self, image: Optional[Union[Base64String, bytes]]=None, only_central_face=False, local_vars_configuration=None):
+    def __init__(self, image: Union[Base64String, bytes], only_central_face=False, local_vars_configuration=None):
+        if not image:
+            raise ApiValueError(f"image: expected <not empty> - obtained <{image}>", "image")
+
         if isinstance(image, bytes):
             image = bytes_to_base64(image)
 
