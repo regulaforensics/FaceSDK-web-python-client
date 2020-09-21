b2553b0f4607525a267272352e788e9abab2ddde
alshund <shundrikaleksey@gmail.com>
2020-09-21 13:07:07 +0300

    rl-440/detect Detect: - add type hints for ext.MatchingApi compare and detect method

--- regula/facerecognition/webclient/ext/api/matching_api.py
+++ regula/facerecognition/webclient/ext/api/matching_api.py
@@ -1,5 +1,6 @@
 from regula.facerecognition.webclient.gen import MatchingApi as GenMatchingApi, Configuration, ApiClient, \
     CompareResponse, DetectResponse
+from regula.facerecognition.webclient.ext import CompareRequest, DetectRequest
 
 
 class MatchingApi(GenMatchingApi):
@@ -19,34 +20,8 @@ class MatchingApi(GenMatchingApi):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.api_client.close()
 
-    def compare(self, compare_request, **kwargs) -> CompareResponse:
+    def compare(self, compare_request: CompareRequest, **kwargs) -> CompareResponse:
         return super().compare(compare_request, **kwargs)
 
-    def detect(self, detect_request, **kwargs) -> DetectResponse:
-        """Detect facial coordinates.
-
-        :param detect_request: (required)
-        :type detect_request: DetectRequest
-        :param async_req: Whether to execute the request asynchronously.
-        :type async_req: bool, optional
-        :param _return_http_data_only: response data without head status code
-                                       and headers
-        :type _return_http_data_only: bool, optional
-        :param _preload_content: if False, the urllib3.HTTPResponse object will
-                                 be returned without reading/decoding response
-                                 data. Default is True.
-        :type _preload_content: bool, optional
-        :param _request_timeout: timeout setting for this request. If one
-                                 number provided, it will be total request
-                                 timeout. It can also be a pair (tuple) of
-                                 (connection, read) timeouts.
-        :param _request_auth: set to override the auth_settings for an a single
-                              request; this effectively ignores the authentication
-                              in the spec for a single request.
-        :type _request_auth: dict, optional
-        :return: Returns the result object.
-                 If the method is called asynchronously,
-                 returns the request thread.
-        :rtype: tuple(DetectResponse, status_code(int), headers(HTTPHeaderDict))
-        """
+    def detect(self, detect_request: DetectRequest, **kwargs) -> DetectResponse:
         return super().detect(detect_request, **kwargs)
