9404295d6febcf17d108b9e668d279039fe642ca
alshund <shundrikaleksey@gmail.com>
2020-09-23 15:07:39 +0300

    rl-440/detect Detect: - add landmarks, roi, landmarkType, detectorType descriptions

--- regula/facerecognition/webclient/gen/models/detect_response.py
+++ regula/facerecognition/webclient/gen/models/detect_response.py
@@ -87,7 +87,7 @@ class DetectResponse(object):
     def detector_type(self):
         """Gets the detector_type of this DetectResponse.  # noqa: E501
 
-        The internal type of detector. Used to calculate the rectangular area of the detected face (roi).  # noqa: E501
+        Internal.  # noqa: E501
 
         :return: The detector_type of this DetectResponse.  # noqa: E501
         :rtype: int
@@ -98,7 +98,7 @@ class DetectResponse(object):
     def detector_type(self, detector_type):
         """Sets the detector_type of this DetectResponse.
 
-        The internal type of detector. Used to calculate the rectangular area of the detected face (roi).  # noqa: E501
+        Internal.  # noqa: E501
 
         :param detector_type: The detector_type of this DetectResponse.  # noqa: E501
         :type detector_type: int
@@ -110,7 +110,7 @@ class DetectResponse(object):
     def landmarks_type(self):
         """Gets the landmarks_type of this DetectResponse.  # noqa: E501
 
-        The internal type of detector. Used to calculate the main coordinate of the detected face (landmarks).  # noqa: E501
+        Internal.  # noqa: E501
 
         :return: The landmarks_type of this DetectResponse.  # noqa: E501
         :rtype: int
@@ -121,7 +121,7 @@ class DetectResponse(object):
     def landmarks_type(self, landmarks_type):
         """Sets the landmarks_type of this DetectResponse.
 
-        The internal type of detector. Used to calculate the main coordinate of the detected face (landmarks).  # noqa: E501
+        Internal.  # noqa: E501
 
         :param landmarks_type: The landmarks_type of this DetectResponse.  # noqa: E501
         :type landmarks_type: int
