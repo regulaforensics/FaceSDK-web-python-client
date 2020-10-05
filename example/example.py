71807e5831b5334b0a4a7d222ce9b355a98849f8
alshund <shundrikaleksey@gmail.com>
2020-10-05 15:32:05 +0300

    rl-90/liveness Liveness: - add liveness bl

--- example/example.py
+++ example/example.py
@@ -1,7 +1,7 @@
 import os
 
-from regula.facerecognition.webclient.ext import MatchingApi, CompareRequest, CompareImage, DetectRequest
-from regula.facerecognition.webclient.gen import ImageSource
+from regula.facerecognition.webclient import ext
+from regula.facerecognition.webclient import gen
 
 api_base_patch = os.getenv("API_BASE_PATH", "http://0.0.0.0:41101/api")
 
@@ -11,15 +11,27 @@ with open("face1.jpg", "rb") as f:
 with open("face2.jpg", "rb") as f:
     face_2_bytes = f.read()
 
+with open("real_video_1.mp4", "rb") as f:
+    real_video_1_bytes = f.read()
 
-with MatchingApi(host=api_base_patch) as api:
+with open("liveness_real_frame_1.jpg", "rb") as f:
+    real_frame_1_bytes = f.read()
+
+with open("liveness_real_depth_1.png", "rb") as f:
+    real_depth_1_bytes = f.read()
+
+with open("liveness_real_image_1.png", "rb") as f:
+    real_image_1_bytes = f.read()
+
+
+with ext.Sdk(host=api_base_patch) as sdk:
     images = [
-        CompareImage(index=1, data=face_1_bytes, type=ImageSource.LIVE),
-        CompareImage(index=2, data=face_1_bytes, type=ImageSource.DOCUMENT_RFID),
-        CompareImage(index=3, data=face_2_bytes),
+        ext.CompareImage(index=1, data=face_1_bytes, type=gen.ImageSource.LIVE),
+        ext.CompareImage(index=2, data=face_1_bytes, type=gen.ImageSource.DOCUMENT_RFID),
+        ext.CompareImage(index=3, data=face_2_bytes),
     ]
-    compare_request = CompareRequest(images=images)
-    compare_response = api.compare(compare_request)
+    compare_request = ext.CompareRequest(images=images)
+    compare_response = sdk.matching_api.compare(compare_request)
 
     print("-----------------------------------------------------------------")
     print("                         Compare Results                         ")
@@ -28,13 +40,42 @@ with MatchingApi(host=api_base_patch) as api:
         print(f"pair({i.first_index}, {i.second_index}) similarity: {i.similarity}")
     print("-----------------------------------------------------------------")
 
-    detect_request = DetectRequest(face_1_bytes)
-    detect_response = api.detect(detect_request)
-    print("                         Detect Results                         ")
+    detect_request = ext.DetectRequest(face_1_bytes)
+    detect_response = sdk.matching_api.detect(detect_request)
+    print("                         Detect Results                          ")
     print("-----------------------------------------------------------------")
     print(f"detector_type: {detect_response.detector_type}")
     print(f"landmark_type: {detect_response._landmarks_type}")
     for i in detect_response.detections:
         print(f"landmarks: {i.landmarks}")
         print(f"roi: {i.roi}")
+    print("-----------------------------------------------------------------")
+
+    video_liveness_result = sdk.liveness_api.check_video_liveness(real_video_1_bytes)
+    print("                   Check video liveness result                   ")
+    print("-----------------------------------------------------------------")
+    print(f"liveness_status: {video_liveness_result.liveness_status}")
+    print("-----------------------------------------------------------------")
+
+    depth_liveness_real_item = ext.DepthLivenessItem(
+        data_scene=real_frame_1_bytes, data_depth=real_depth_1_bytes, depth_scale=0.001
+    )
+    depth_liveness = gen.DepthLiveness(images=[depth_liveness_real_item])
+    depth_liveness_result = sdk.liveness_api.check_depth_liveness(depth_liveness=depth_liveness)
+    print("                   Check depth liveness result                   ")
+    print("-----------------------------------------------------------------")
+    for i in depth_liveness_result:
+        print(f"index: {i.index}")
+        print(f"code: {i.code}")
+        print(f"liveness_status: {i.liveness_status}")
+    print("-----------------------------------------------------------------")
+
+    image_liveness_item = ext.ImageLivenessItem(data=real_image_1_bytes)
+    image_liveness = gen.ImageLiveness(images=[image_liveness_item])
+    sdk.liveness_api.check_image_liveness(image_liveness=image_liveness)
+    print("                   Check image liveness result                   ")
+    for i in depth_liveness_result:
+        print(f"index: {i.index}")
+        print(f"code: {i.code}")
+        print(f"liveness_status: {i.liveness_status}")
     print("-----------------------------------------------------------------")
--- regula/facerecognition/webclient/ext/models/__init__.py
+++ regula/facerecognition/webclient/ext/models/__init__.py
@@ -1,3 +1,5 @@
 from regula.facerecognition.webclient.ext.models.compare_request import CompareRequest
 from regula.facerecognition.webclient.ext.models.compare_image import CompareImage
+from regula.facerecognition.webclient.ext.models.depth_liveness_item import DepthLivenessItem
 from regula.facerecognition.webclient.ext.models.detect_request import DetectRequest
+from regula.facerecognition.webclient.ext.models.image_liveness_item import ImageLivenessItem
