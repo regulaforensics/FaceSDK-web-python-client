a586ed7f506ba7aa2861c3d8000bfe23daf4428d
Hleb Albau <hleb.albau@gmail.com>
2021-02-09 13:52:09 +0300

    Update update-models.sh

--- update-models.sh
+++ update-models.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker run --rm -v "${PWD}:/client" -v "${PWD}/../FaceRecognition-web-openapi:/definitions" \
-openapitools/openapi-generator-cli generate -g python \
+openapitools/openapi-generator-cli:v5.0.0-beta2 generate -g python \
 -i /definitions/index.yml -o /client -c /client/generator-config.json \
 -t /client/generator-templates
