1a2a160b8f8d0d02f5498fa2c99113d7f52c9d35
GitHub Action <action@github.com>
2022-07-27 16:57:04 +0000

    update-clients

--- regula/facesdk/webclient/gen/api/__init__.py
+++ regula/facesdk/webclient/gen/api/__init__.py
@@ -3,4 +3,7 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
+from regula.facesdk.webclient.gen.api.group_api import GroupApi
 from regula.facesdk.webclient.gen.api.matching_api import MatchingApi
+from regula.facesdk.webclient.gen.api.person_api import PersonApi
+from regula.facesdk.webclient.gen.api.search_api import SearchApi
