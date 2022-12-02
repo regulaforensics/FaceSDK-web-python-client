9a9e10796f2d3af079c02f9aa823b913d3616ec3
Dan Gost <Danila Kislitsyn>
2022-12-02 13:06:05 +0300

    output the match&search results

--- example/example_identification_module.py
+++ example/example_identification_module.py
@@ -1,12 +1,15 @@
+import base64
 import os
 
 from regula.facesdk.webclient.ext import FaceSdk
 from regula.facesdk.webclient.gen.model.image_fields_image import ImageFieldsImage
+from regula.facesdk.webclient.gen.model.image_source import ImageSource
+from regula.facesdk.webclient.gen.model.match_and_search_request_all_of_images import MatchAndSearchRequestAllOfImages
 from regula.facesdk.webclient.gen.model.person_fields import PersonFields
 from regula.facesdk.webclient.gen.model.search_request import SearchRequest
 from regula.facesdk.webclient.gen.model.update_group import UpdateGroup
 
-api_base_path = os.getenv("API_BASE_PATH", "http://127.0.0.0:41101")
+api_base_path = os.getenv("API_BASE_PATH", "http://127.0.0.1:41101")
 
 with open("face1.jpg", "rb") as f:
     face_1_bytes = f.read()
@@ -45,3 +48,11 @@ print(f"Person #1 {int(person1.id)} {person1.name}")
 print(f"Person #2 {int(person2.id)} {person2.name}")
 print(f"Group {int(group.id)} {group.name}")
 print(result)
+
+match_and_search = sdk.matching_api.match_and_search(
+    images=[MatchAndSearchRequestAllOfImages(base64.b64encode(face_1_bytes).decode("UTF-8"), ImageSource.LIVE),
+            MatchAndSearchRequestAllOfImages(base64.b64encode(face_2_bytes).decode("UTF-8"), ImageSource.LIVE)],
+    group_ids=[group.id]
+)
+
+print(f"{match_and_search}")
