061015ca71ee49f9cb802523e90a1edfd4a0a25c
Dan Gost <Danila Kislitsyn>
2022-12-02 13:44:24 +0300

    improve example

--- regula/facesdk/webclient/ext/api/matching_api.py
+++ regula/facesdk/webclient/ext/api/matching_api.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from regula.facesdk.webclient.gen import Configuration, ApiClient
 from regula.facesdk.webclient.gen.apis import MatchingApi as GenMatchingApi
 from regula.facesdk.webclient.ext.models import MatchRequest, DetectRequest
 from regula.facesdk.webclient.gen.model.detect_response import DetectResponse
 from regula.facesdk.webclient.gen.model.match_and_search_request import MatchAndSearchRequest
+from regula.facesdk.webclient.gen.model.match_and_search_request_all_of_images import MatchAndSearchRequestAllOfImages
 from regula.facesdk.webclient.gen.model.match_and_search_response import MatchAndSearchResponse
 from regula.facesdk.webclient.gen.model.match_response import MatchResponse
 
@@ -24,8 +27,15 @@ class MatchingApi(GenMatchingApi):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.api_client.close()
 
-    def match_and_search(self, match_and_search_request: MatchAndSearchRequest, **kwargs) -> MatchAndSearchResponse:
-        return super().match_and_search(match_and_search_request)
+    def match_and_search(self, images: List[MatchAndSearchRequestAllOfImages], group_ids: List[int],
+                         limit: int = 100, threshold: float = 1.0) -> MatchAndSearchResponse:
+        body = MatchAndSearchRequest(
+            images=images,
+            limit=limit,
+            threshold=1.0,
+            group_ids=group_ids
+        )
+        return super().match_and_search(body)
 
     def match(self, match_request: MatchRequest, **kwargs) -> MatchResponse:
         return super().match(match_request, **kwargs)
