7499601442e02da2812d2bbb92ecf8623cb9a01a
inecsor <petr.shumsky@gmail.com>
2023-12-13 17:29:42 +0300

    improved misc images

--- tests/misc/paths_and_urls.py
+++ tests/misc/paths_and_urls.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import pytest
 from regula.facesdk.webclient.ext import FaceSdk
@@ -12,19 +13,26 @@ files_path = os.path.join(project_dir, 'files')
 
 FACE_1_PATH = os.path.join(files_path, "face1.jpg")
 FACE_2_PATH = os.path.join(files_path, "face2.jpg")
-LIVE_PHOTO_PATH = os.path.join(files_path, "me.png")
-PRINTED_DOCUMENT_PATH = os.path.join(files_path, "printedDoc.png")
-DOCUMENT_WITH_LIVE_PATH = os.path.join(files_path, "me_and_id.png")
+LIVE_PHOTO_PATH = os.path.join(files_path, "docFace.txt")
+PRINTED_DOCUMENT_PATH = os.path.join(files_path, "printedDoc.txt")
+DOCUMENT_WITH_LIVE_PATH = os.path.join(files_path, "docFace_and_printedDoc.txt")
 SEVERAL_FACES_IMAGE_PATH = os.path.join(files_path, "severalFaces.jpg")
 
 
 def read_image_bytes(image_path):
-    with open(image_path, "rb") as f:
-        return f.read()
+    _, file_extension = os.path.splitext(image_path)
+
+    if file_extension.lower() == '.txt':  # Assuming the Base64 file has a .txt extension
+        with open(image_path, "r") as f:
+            base64_data = f.read()
+            return base64.b64decode(base64_data)
+    else:  # For other file types, assume they are images
+        with open(image_path, "rb") as f:
+            return f.read()
 
 
 def create_dictionary(response):
-    # We need this function because a response object is not subscriptable
+    # We need this function because a response object is not subscript
     response_dict = response.to_dict() if hasattr(response, 'to_dict') else response
     return response_dict
 
