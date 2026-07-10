17cd5767d4d2c4cef49ba96be8ebb4f69525bd6d
GitHub Action <action@github.com>
2026-07-10 18:12:32 +0000

    Commit:

--- regula/facesdk/webclient/gen/models/search_request.py
+++ regula/facesdk/webclient/gen/models/search_request.py
@@ -34,7 +34,7 @@ class SearchRequest(BaseModel):
     output_image_params: SkipValidation[Optional[OutputImageParams]] = Field(alias="outputImageParams", default=None)
     detect_all: SkipValidation[Optional[bool]] = Field(alias="detectAll", default=None, description="Whether to detect all faces in the image. If set to `false`, only the most central face is detected.")
     threshold: SkipValidation[Optional[float]] = Field(alias="threshold", default=None, description="The similarity threshold.")
-    limit: SkipValidation[Optional[int]] = Field(alias="limit", default=None, description="The maximum number of results to be returned.")
+    limit: SkipValidation[Optional[int]] = Field(alias="limit", default=None, description="The maximum number of results to be returned. If not specified, the default value is 100.")
     __properties: ClassVar[List[str]] = ["tenant", "env", "createPerson", "groupIds", "filter", "tag", "image", "outputImageParams", "detectAll", "threshold", "limit"]
 
     model_config = ConfigDict(
